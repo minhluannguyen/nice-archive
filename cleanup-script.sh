df3d33c51d73ea92fbd4bba2f5dae4678c4938ab
Minh Luân <minh-luan.nguyen@telecom-sudparis.eu>
2026-06-23 18:37:15 +0200

    update clean up script

--- cleanup-script.sh
+++ cleanup-script.sh
@@ -6,3 +6,4 @@ DIR="$1"
 find "$DIR" -type f -name "*.qcow2" -delete
 find "$DIR" -type l -name "*result" -delete
 find "$DIR" -type f -name ".nixos-test-history" -delete
+find "$DIR" -type f -name "*.log" -delete
